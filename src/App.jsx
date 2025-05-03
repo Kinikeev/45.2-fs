ab3a59dfb9a752b41d6bd0e8fce20df9a6a8bf88
Kinikeev <kinikeev@yahoo.de>
2025-05-03 11:33:52 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import "./App.css";
+import Lesson01 from "./lessons/lesson_01/Lesson01";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Lesson01 />
+    </>
+  );
+}
+
+export default App;
