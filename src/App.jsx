ebf7ff4cb166940f17401f2dea245c3461ff4001
Kinikeev <kinikeev@yahoo.de>
2025-05-06 22:54:04 +0200

    react lesson 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import { useState } from 'react'
 
 import "./App.css";
-import Lesson01 from "./lessons/lesson_01/Lesson01";
+import Lesson02 from "./lessons/lesson02/Lesson02";
+import Lesson01 from "./lessons/lesson01/Lesson01";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Lesson01 />
+      {/* <Lesson01 /> */}
+      <Lesson02 />
     </>
   );
 }
